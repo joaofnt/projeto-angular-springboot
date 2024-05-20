79ce2ce0f2f7cd1e31d5d3874f283904bf019910
João Neto <joao.fnnt@gmail.com>
2024-05-20 18:32:03 -0300

    criando as interfaces repositories

---
+++ bookstore/src/main/java/com/joao/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.joao.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.joao.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Long>{
+
+}
